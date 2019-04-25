4a839432a9d05d3a61e0fd1e92cb42c26ffb3533
Brock Taban <hello@brocktaban.com>
2019-04-25 19:01:50 -0400

    Fetched Tweets by Current Location

--- app/src/main/java/com/example/twitterlocationapp/MapsActivity.kt
+++ app/src/main/java/com/example/twitterlocationapp/MapsActivity.kt
@@ -1,18 +1,20 @@
 package com.example.twitterlocationapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import android.location.LocationManager
-import android.util.Log
 import android.widget.Toast
-
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.gms.maps.*
+import org.json.JSONObject
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,
@@ -28,12 +30,61 @@ class MapsActivity : AppCompatActivity(), GoogleMap.OnMyLocationButtonClickListe
         setContentView(R.layout.activity_maps)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
 
         getCurrentLocation()
+        getTweetsByLocation()
+
+        mapFragment.getMapAsync(this)
     }
 
+    private fun getTweetsByLocation() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.twitter.com/1.1/search/tweets.json?geocode=${latitude},${longitude},5km&result_type=mixed&count=100"
+
+        val stringRequest = object : StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    val jsonObj = JSONObject(response.toString())
+                    val jsonArray = jsonObj.getJSONArray("statuses")
+
+                    for (i in 0 until jsonArray.length()) {
+                        val statuses = jsonArray.getJSONObject(i)
+
+                        val user = statuses.getJSONObject("user")
+
+                        val userName =  user.getString("name")
+                        val tweet = statuses.getString("text")
+
+                        if (statuses.get("coordinates").toString() != "null") {
+                            val c = statuses.getJSONObject("coordinates").getJSONArray("coordinates")
+
+                            val latLng = LatLng(c.getDouble(1), c.getDouble(0))
+                            mMap.addMarker(MarkerOptions().position(latLng).title("by $userName").snippet(tweet))
+                        }
+
+                        if (statuses.get("place").toString() != "null") {
+                            val place = statuses.getJSONObject("place")
+
+                            val box = place.getJSONObject("bounding_box")
+                            val c = box.getJSONArray("coordinates").getJSONArray(0).getJSONArray(0)
+
+                            val latLng = LatLng(c.getDouble(1), c.getDouble(0))
+                            mMap.addMarker(MarkerOptions().position(latLng).title("by $userName").snippet(tweet))
+                        }
+                    }
+                },
+                Response.ErrorListener { Toast.makeText(this, "That didn't work!", Toast.LENGTH_SHORT).show() }) {
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["authorization"] = "Bearer ${getString(R.string.twitter_bearer)}"
+                return params
+            }
+        }
+
+        queue.add(stringRequest)
+    }
+
+    @SuppressLint("MissingPermission")
     private fun getCurrentLocation() {
         val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
@@ -54,10 +105,11 @@ class MapsActivity : AppCompatActivity(), GoogleMap.OnMyLocationButtonClickListe
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val yourLocation = LatLng(latitude, longitude)
+        val currentLocation = LatLng(latitude, longitude)
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(yourLocation))
-        mMap.animateCamera( CameraUpdateFactory.zoomTo( 13.0f ) )
+        val location = CameraUpdateFactory.newLatLngZoom(currentLocation, 12.0f)
+        mMap.animateCamera(location)
+
         mMap.isMyLocationEnabled = true
         mMap.setOnMyLocationButtonClickListener(this)
         mMap.setOnMyLocationClickListener(this)
